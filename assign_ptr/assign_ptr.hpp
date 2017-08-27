59e9c9bd59883c398387ad3e4626e80c9ea814e6
Zachary Struck <zachstruck@gmail.com>
2017-08-27 16:03:11 +0700

    Implement assign_ptr

--- assign_ptr/assign_ptr.hpp
+++ assign_ptr/assign_ptr.hpp
@@ -1,4 +1,65 @@
 #ifndef ZPP_ASSIGN_PTR_HPP
 #define ZPP_ASSIGN_PTR_HPP
 
+#include <type_traits>
+
+namespace zpp
+{
+    namespace detail
+    {
+        template <typename SmartPtr>
+        class assign_ptr_impl
+        {
+        public:
+            using element_type = typename std::remove_extent_t<typename SmartPtr::element_type>;
+
+            // No default construction
+            assign_ptr_impl() = delete;
+
+            explicit assign_ptr_impl(SmartPtr& p) noexcept :
+                pp_(&p)
+            {
+            }
+
+            // No copy semantics
+            assign_ptr_impl(assign_ptr_impl const&) = delete;
+            assign_ptr_impl& operator=(assign_ptr_impl const&) = delete;
+
+            // NOTE
+            // Ideally, no move semantics, but for now...
+            //
+            // TODO  C++17
+            // When guaranteed copy elision is implemented from P0135R1,
+            // delete the move constructor
+            assign_ptr_impl(assign_ptr_impl&&) = default;
+            assign_ptr_impl& operator=(assign_ptr_impl&&) = delete;
+
+            ~assign_ptr_impl()
+            {
+                pp_->reset(p_);
+            }
+
+            operator element_type*&() && noexcept
+            {
+                return p_;
+            }
+
+            operator element_type**() && noexcept
+            {
+                return &p_;
+            }
+
+        private:
+            SmartPtr* pp_ = nullptr;
+            element_type* p_ = nullptr;
+        };
+    }
+
+    template <typename SmartPtr>
+    detail::assign_ptr_impl<SmartPtr> assign_ptr(SmartPtr& p)
+    {
+        return detail::assign_ptr_impl<SmartPtr>(p);
+    }
+}
+
 #endif
--- assign_ptr_test/assign_ptr_test.cpp
+++ assign_ptr_test/assign_ptr_test.cpp
@@ -1,6 +1,252 @@
+#include "assign_ptr.hpp"
+
 #include "Catch\single_include\catch.hpp"
 
-TEST_CASE("test")
+#include <cstddef>
+#include <memory>
+#include <new>
+
+// Single item
+
+// Reference to pointer
+
+namespace
 {
-    CHECK(1 == 1);
+    void foo_ref_ptr(int*& p)
+    {
+        p = new int(42);
+    }
+}
+
+TEST_CASE("Allocate single item through reference output parameter")
+{
+    SECTION("Raw pointer")
+    {
+        int* p = nullptr;
+        foo_ref_ptr(p);
+
+        REQUIRE(p != nullptr);
+        CHECK(*p == 42);
+
+        delete p;
+    }
+
+    SECTION("auto_ptr")
+    {
+        std::auto_ptr<int> p;
+        foo_ref_ptr(zpp::assign_ptr(p));
+
+        REQUIRE(p.get() != nullptr);
+        CHECK(*p == 42);
+    }
+
+    SECTION("unique_ptr")
+    {
+        std::unique_ptr<int> p;
+        foo_ref_ptr(zpp::assign_ptr(p));
+
+        REQUIRE(p != nullptr);
+        CHECK(*p == 42);
+    }
+
+    SECTION("shared_ptr")
+    {
+        std::shared_ptr<int> p;
+        foo_ref_ptr(zpp::assign_ptr(p));
+
+        REQUIRE(p != nullptr);
+        CHECK(*p == 42);
+    }
+}
+
+// Pointer to a pointer
+
+namespace
+{
+    void foo_ptr_ptr(int** p)
+    {
+        *p = new int(42);
+    }
+}
+
+TEST_CASE("Allocate single item through pointer output parameter")
+{
+    SECTION("Raw pointer")
+    {
+        int* p = nullptr;
+        foo_ptr_ptr(&p);
+
+        REQUIRE(p != nullptr);
+        CHECK(*p == 42);
+
+        delete p;
+    }
+
+    SECTION("auto_ptr")
+    {
+        std::auto_ptr<int> p;
+        foo_ptr_ptr(zpp::assign_ptr(p));
+
+        REQUIRE(p.get() != nullptr);
+        CHECK(*p == 42);
+    }
+
+    SECTION("unique_ptr")
+    {
+        std::unique_ptr<int> p;
+        foo_ptr_ptr(zpp::assign_ptr(p));
+
+        REQUIRE(p != nullptr);
+        CHECK(*p == 42);
+    }
+
+    SECTION("shared_ptr")
+    {
+        std::shared_ptr<int> p;
+        foo_ptr_ptr(zpp::assign_ptr(p));
+
+        REQUIRE(p != nullptr);
+        CHECK(*p == 42);
+    }
+}
+
+// Array
+
+// Reference to pointer
+
+namespace
+{
+    void foo_ref_array(int*& p, std::size_t& len)
+    {
+        len = 3;
+
+        p = new int[len];
+
+        for (std::size_t i = 0; i < len; ++i)
+        {
+            p[i] = i;
+        }
+    }
+}
+
+TEST_CASE("Allocate array through reference output parameter")
+{
+    SECTION("Raw pointer")
+    {
+        int* p = nullptr;
+        std::size_t len = 0;
+        foo_ref_array(p, len);
+
+        REQUIRE(p != nullptr);
+        REQUIRE(len == 3);
+        CHECK(p[0] == 0);
+        CHECK(p[1] == 1);
+        CHECK(p[2] == 2);
+
+        delete[] p;
+    }
+
+    SECTION("auto_ptr")
+    {
+        // std::auto_ptr does not support arrays
+    }
+
+    SECTION("unique_ptr")
+    {
+        std::unique_ptr<int[]> p;
+        std::size_t len = 0;
+        foo_ref_array(zpp::assign_ptr(p), len);
+
+        REQUIRE(p != nullptr);
+        REQUIRE(len == 3);
+        CHECK(p[0] == 0);
+        CHECK(p[1] == 1);
+        CHECK(p[2] == 2);
+    }
+
+    SECTION("shared_ptr")
+    {
+        // TODO  C++17
+        // std::shared_ptr does not yet support arrays
+        /*
+        std::shared_ptr<int[]> p;
+        std::size_t len = 0;
+        foo_ref_array(zpp::assign_ptr(p), len);
+
+        REQUIRE(p != nullptr);
+        REQUIRE(len == 3);
+        CHECK(p[0] == 0);
+        CHECK(p[1] == 1);
+        CHECK(p[2] == 2);
+        */
+    }
+}
+
+// Pointer to pointer
+
+namespace
+{
+    void foo_ptr_array(int** p, std::size_t& len)
+    {
+        len = 3;
+
+        *p = new int[len];
+
+        for (std::size_t i = 0; i < len; ++i)
+        {
+            (*p)[i] = i;
+        }
+    }
+}
+
+TEST_CASE("Allocate array through pointer output parameter")
+{
+    SECTION("Raw pointer")
+    {
+        int* p = nullptr;
+        std::size_t len = 0;
+        foo_ptr_array(&p, len);
+
+        REQUIRE(p != nullptr);
+        REQUIRE(len == 3);
+        CHECK(p[0] == 0);
+        CHECK(p[1] == 1);
+        CHECK(p[2] == 2);
+
+        delete[] p;
+    }
+
+    SECTION("auto_ptr")
+    {
+        // std::auto_ptr does not support arrays
+    }
+
+    SECTION("unique_ptr")
+    {
+        std::unique_ptr<int[]> p;
+        std::size_t len = 0;
+        foo_ptr_array(zpp::assign_ptr(p), len);
+
+        REQUIRE(p != nullptr);
+        REQUIRE(len == 3);
+        CHECK(p[0] == 0);
+        CHECK(p[1] == 1);
+        CHECK(p[2] == 2);
+    }
+
+    SECTION("shared_ptr")
+    {
+        // TODO  C++17
+        /*
+        std::shared_ptr<int[]> p;
+        std::size_t len = 0;
+        foo_ptr_array(zpp::assign_ptr(p), len);
+
+        REQUIRE(p != nullptr);
+        REQUIRE(len == 3);
+        CHECK(p[0] == 0);
+        CHECK(p[1] == 1);
+        CHECK(p[2] == 2);
+        */
+    }
 }
